58551f161fd3dd5ba94463e09d9de4d3ae1435ea
Abdullah Sultan Barizy <abdullahsultanbarizy@gmail.com>
2026-09-07 00:25:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalculatorController;
+use App\Http\Controllers\PageController;
 
-Route::inertia('/', 'Welcome')->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/project-idea', [PageController::class, 'project'])->name('project');
 Route::get('/hitung/{a?}/{b?}/{operation?}', [CalculatorController::class, 'calculate'])->name('calculator');
